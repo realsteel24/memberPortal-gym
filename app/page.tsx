6f5732929f5afb853656ead4c742c52b14a61b93
Realsteel <singhrohan.1996@gmail.com>
2024-12-25 18:41:13 +0530

    inital commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState,  } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
